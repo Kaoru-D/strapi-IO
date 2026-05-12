f5261b3f1d8fc7808725201b025de8b3ca92fbeb
DanielK <daniel.rt5d@gmail.com>
2026-05-12 11:17:10 -0500

    feat: implement Hero component and add global styles for navigation and layout

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className='hero-container'>
+      <div className="hero-center">
+        <h1>
+          Manejo de contenido <br />
+          de manera eficiente
+        </h1>
+        <p>
+          Strapi es un sistema de gestión de contenido de código abierto que facilita la creación, gestión y distribución de contenido digital. Con su interfaz intuitiva y su flexibilidad, Strapi permite a los desarrolladores y creadores de contenido construir aplicaciones web y móviles de manera rápida y eficiente.
+        </p>
+      </div>
+    </div>
   )
 }
 
